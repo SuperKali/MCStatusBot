da8ffc3798b18a5d3787a7ba70a223db5456ace1
SuperKali <hello@superkali.me>
2022-06-14 12:55:31 +0200

    Under development addnewserver command

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,3 +1,4 @@
+from asyncio_dgram import connect
 import discord
 import json
 from discord.ext import commands
@@ -18,7 +19,7 @@ class Commands(commands.Cog):
 
             embed = discord.Embed(
                 title="MCStatusBot Configured 🎉", 
-                description=f"This message will be updated with the status message automatically.", 
+                description="This message will be updated with the status message automatically.", 
                 color=discord.Colour.blue())
 
             message = await ctx.send(embed=embed)
@@ -29,7 +30,7 @@ class Commands(commands.Cog):
             json.dump(self.data, data_save)
             data_save.close() 
         else:
-            ctx.send("MCStatusBot: You don't have the permission for use this command, only the owner can do this command.")   
+            ctx.send("⛔️ **[MCStatusBot]** You don't have the permission for use this command, only the owner can do this command.")   
 
 
     @commands.command()
@@ -42,6 +43,24 @@ class Commands(commands.Cog):
         embed.set_footer(text="Bot developed by SuperKali#8716")    
         
         await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def addnewserver(self, ctx, name: str, address: str, port: int, is_bedrock: bool):
+        # STILL UNDER DEVELOPMENT
+        if ctx.message.author.id == self.config['owner_id']:
+            with open('config.json') as config:
+                self.config = json.load(config)
+
+            if name is None:
+                embed = discord.Embed(
+                    title="MCStatusBot Example for add servers",
+                    description=f"Use the following format: {config['bot_prefix']}addnewserver <server_name> <server_address> <server_port> <is_bedrock>",
+                    color=discord.Colour.dark_blue())
+
+            
+            message = await ctx.send(embed=embed)
+            await ctx.message.delete()
+    
 
 def setup(client):
     client.add_cog(Commands(client))
